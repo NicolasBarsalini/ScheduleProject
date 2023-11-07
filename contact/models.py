a8c261512cf0c34b88f93ec39cb5fc6dfc150634
Nicolas <nicolasbarsalini2017@gmail.com>
2023-11-07 18:11:56 -0300

    Created the contact table

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Contact(models.Model): #herda do models model, sintetizo uma tabela a partir disso
+    first_name = models.CharField(max_length=50) #sao por padrao obrigatorios caso estejam cadastrados aqui
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50) #o campo se torna opcional
+    email = models.EmailField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now()) #valor padrao ao registrar no banc de dados
+    description = models.TextField(blank=True)
+    #uma string na base de dados do tipo charfield eh limitada um text field nao tenho q passar a limitacao
+    #id n precisa colocar pois eh automatico
+    #criar uma migration sempre ao editar o model
